b2d7a6621393a3264f14907faabd64c27aa0cb17
Asim Zahid <asimzahid02@gmail.com>
2019-11-02 17:17:55 -0400

    input and some other string maipulations

---
+++ string_manipulation.py
@@ -0,0 +1,9 @@
+#simple program to say hello and ask for name
+print("Hello!") #Greetings
+print("What is your name?") #name
+name = input()
+print("It is good to meet you, " + name)
+print("The length of your name is:" + str(len(name)))
+print("What is your age?")
+age = input()
+print('You will  be '+ str(int(age) + 1) +  ' in a  year')
