dd3ac8648e3106b3159f9ffe542617242077a695
Asim Zahid <asimzahid02@gmail.com>
2019-11-02 19:49:17 -0400

    forLoop.py updated, 100 sum program written

---
+++ 100sum.py
@@ -0,0 +1,4 @@
+total = 0
+for num in range(101):
+	total += num
+print(total)
---
+++ forLoop.py
@@ -0,0 +1,4 @@
+print('My name is')
+#for index in range(start, upto end, jump)
+for i in range(1, 20, 5):
+	print('ju=imy five times ' + str(i))
